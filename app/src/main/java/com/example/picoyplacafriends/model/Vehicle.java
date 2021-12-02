4303e193a527c14f1ef6355fcf2d927d9c72d043
camilovivas <camilo-152000@hotmail.com>
2021-12-02 09:15:19 -0500

    edit view of register vehicle

--- app/src/main/java/com/example/picoyplacafriends/model/Vehicle.java
+++ app/src/main/java/com/example/picoyplacafriends/model/Vehicle.java
@@ -2,4 +2,21 @@ package com.example.picoyplacafriends.model;
 
 public class Vehicle {
     private String placa;
+    private String tarjetaPropiedadId;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String placa, String tarjetaPropiedadId) {
+        this.placa = placa;
+        this.tarjetaPropiedadId = tarjetaPropiedadId;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getTarjetaPropiedadId() {
+        return tarjetaPropiedadId;
+    }
 }
