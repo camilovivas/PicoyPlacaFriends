6cc1a5899575d4face90c0769bbf6b4a922481aa
Elías Estupiñan <eliaset096@gmail.com>
2021-12-08 17:02:42 -0500

    ConfigurationCenterActivity Added

---
+++ app/src/main/java/com/example/picoyplacafriends/configuration/ConfigurationCenterActivity.java
@@ -0,0 +1,17 @@
+package com.example.picoyplacafriends.configuration;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.picoyplacafriends.R;
+import android.os.Bundle;
+
+public class ConfigurationCenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration_center);
+    }
+
+
+
+}
