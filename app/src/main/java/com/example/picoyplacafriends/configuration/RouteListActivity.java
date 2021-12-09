da55f92aac3dd3aea3c8d717c6da689e0b4cca09
Elías Estupiñan <eliaset096@gmail.com>
2021-12-08 20:55:30 -0500

    Activities and GUI of the configuration Added

---
+++ app/src/main/java/com/example/picoyplacafriends/configuration/RouteListActivity.java
@@ -0,0 +1,14 @@
+package com.example.picoyplacafriends.configuration;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.picoyplacafriends.R;
+import android.os.Bundle;
+
+public class RouteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_list);
+    }
+}
